26a5a37e477ccfff7ad355cf4d24a1976b0fc4dd
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-08-10 11:10:53 +0300

    Extended meta info for classes and unicode methods for model classes

--- apps/pages/models.py
+++ apps/pages/models.py
@@ -4,14 +4,21 @@ from django.db import models
 class Category(models.Model):
 	name = models.CharField(max_length=128,help_text="Category of the page")
 	parent = models.ForeignKey('Category')
+	def __unicode__(self):
+		return self.name
+	class Meta:
+		verbose_name_plural = "categories"
 
 class Content(models.Model):
 	title = models.CharField(null=False, blank=False,max_length=128,help_text="title of the contents")
 	heading = models.CharField(max_length=128,help_text="heading of the contents")
 	mainbody = models.TextField(max_length=128,help_text="text field of the contents")
 	date_posted = models.DateTimeField()
+	def __unicode__(self):
+		return self.title
 	
 class Page(Content):
 	category = models.ForeignKey('Category')
 	author = models.CharField(max_length=128,help_text="Author of the page")
-	
+	def __unicode__(self):
+		return self.title
