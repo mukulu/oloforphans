6af815868e470b682c4b8ba259fdbc0b6082c6c8
Allen <allen.machary@gmail.com>
2010-08-11 15:35:32 +0300

    news app

--- apps/news/models.py
+++ apps/news/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    heading = models.CharField(max_length=256, help_text="Write the title of the news")
+    Content = models.TextField()
+    photo = models.ImageField(null=True, blank=True)
+    created_date = models.DateTimeField(default = datetime.now)
+    
+    def __init__(self):
+        return '%s (%s)' % (self.heading, self.created_date)    
--- urls.py
+++ urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from apps.news.models import *
-from apps.pages.models import *
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -14,6 +11,5 @@ urlpatterns = patterns('',
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
