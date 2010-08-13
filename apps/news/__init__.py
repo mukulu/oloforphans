ba9d8d203ff5831118d5cece83bd946fd0d54546
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-08-13 20:33:26 +0300

    Documented applications, their documentation looks awesome when imported from shell

---
+++ apps/news/__init__.py
@@ -0,0 +1,7 @@
+""" Application for managing news contents posted daily by authorized users and content administrator
+
+This application lets user add, remove and update news to appear on the website,
+the news are database generated, and time controlled, it keeps tracks of who posts
+which news and when was the news posted and when it does expires, the default expiry
+date set is one month.
+"""
