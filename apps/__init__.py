412ac319ed2d6fd7ca5b4e4dd9807e17578c50e2
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-08-13 19:06:24 +0300

    Did major documentation on packages and prompts, ignoring backup files.
    Ignored backup files created by text editors, revised README and TODO
    documented the oloforphans package file, apps package file to document what's kept in.
    Revised manage.py to say what user should do on the fly when settings.py is missing since we're ignoring it.
    Added icons directory, for icons we will put on the website.

---
+++ apps/__init__.py
@@ -0,0 +1,14 @@
+""" Application package holding the admin applications
+
+This package holds all applications that gets loaded into the website
+And the applications displayed at the admin interface.
+
+Applications should be placed in this package and registered in the
+settings file with its urls, and with admin.py if it is to be displayed
+in the admin interface.
+
+For more information please visit http://github.com/allenmachary/oloforphans/
+Authors:
+1. John Francis Mukulu <john.f.mukulu@gmail.com>
+2. Allen Machary <allenmachary@gmail.com>
+"""
--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
+""" Management of the entire oloforphans webiste and its appplications
+That includes database management, development testing, and shell client
+with pre-configred settings.
+"""
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
     import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r.\nPlease create a settings.py file from settings.py.sample adjusting it to your database settings.\nOr you'll have to run django-admin.py, passing it settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow. check it out.)\n" % __file__)
     sys.exit(1)
 
 if __name__ == "__main__":
