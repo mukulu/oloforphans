1d9a7094d30c007a79554c2d8cd7a07c43c23894
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-08-13 13:25:35 +0300

    Trying to adjust the views to better look

--- apps/students/admin.py
+++ apps/students/admin.py
@@ -1,11 +1,34 @@
 from django.contrib import admin
 from apps.students.models import Person, Student
+from django.contrib.auth.models import User
 
 class PersonAdmin(admin.ModelAdmin):
-	list_display=("photo","user","birthdate","birthplace")
+	list_display=("username","firstname","lastname")
+
+class UserInline(admin.TabularInline):
+	model = Student
+	extra = 1
 
 class StudentAdmin(admin.ModelAdmin):
-	list_display=("photo","user","birthdate","birthplace","form")
+	pass
+#	list_display=("photo","user","birthdate","birthplace","form")
+#	inlines = [UserInline,]
+#	fieldsets = (
+#        ('Basic User Information', {
+#            'fields': ('birthdate','birthplace','form','photo')
+#        }),
+#        ('Hobbies and Favorites', {
+#			'classes': ('collapse',),
+#			'fields':('hobbies','favorite_subject','future_dream')
+#		}),
+#		('Detailed informations', {
+#			'classes': ('collapse',),
+#			'fields':('about_her','explanation','quotes')
+#		}),
+#        ('Relationships', {
+#            'classes': ('collapse',),
+#            'fields': ('parents','siblings','guardians',)
+#        }),
+#    )
 
-admin.site.register(Person,PersonAdmin)
 admin.site.register(Student,StudentAdmin)
--- apps/students/models.py
+++ apps/students/models.py
@@ -8,7 +8,7 @@ from apps.school_classes.models import SchoolClass
 # 2. A user class as it has enough info.
 # 	lets just leave most relevant stuff here usable to both.
 # 3. Should the student really be able to login.? and do what.?
-class Person(models.Model):
+class Person(User):
 	"""
 		Holds information about people in the system
 		and collection methods to process those informations
@@ -16,24 +16,24 @@ class Person(models.Model):
 		@I have used PHOTO_DIR declared in settings.py, not sure if it
 		 needs any imports to use settings from settings.py
 	"""
-	user = models.ForeignKey(User, unique=True)
-	photo = models.ImageField(null=True,upload_to=settings.PHOTO_DIR, help_text="Recent Photo")
-	birthdate = models.DateField(help_text="Your date of birth")
-	birthplace = models.CharField(max_length=128, help_text="Place you were born")
-	hobbies = models.TextField(null=True, blank=True,help_text="Activity or interesest you do for pleasure")
-	sibblings = models.TextField(null=True,blank=True,help_text="Your kinsman, next of kin, persons of near relationship")
+	user = models.ForeignKey('Student', unique=True)	
 	
 	def __unicode__(self):
 		return self.user.username
 
-class Student(Person):
+class Student(models.Model):
 	"""
 		@Should we put form for stream, or just an entrance year?
 		if we put entrance year, we might need to know if he/she's o-level
 		or a-level to know which form he is.
 	"""
-	form = models.ForeignKey(SchoolClass,help_text="Form/Stream you are in.")
-	favorite_subject = models.CharField(max_length=128,help_text="Favorite subject")
+	photo = models.ImageField(null=True,blank=True,upload_to=settings.PHOTO_DIR, help_text="Recent Photo")
+	birthdate = models.DateField(help_text="Your date of birth")
+	birthplace = models.CharField(null=True,blank=True,max_length=128, help_text="Place you were born")
+	hobbies = models.TextField(null=True, blank=True,help_text="Activity or interesest you do for pleasure")
+	siblings = models.TextField(null=True,blank=True,help_text="Your kinsman, next of kin, persons of near relationship")
+	form = models.ForeignKey(SchoolClass,null=True,blank=True,help_text="Form/Stream you are in.")
+	favorite_subject = models.CharField(null=True,blank=True,max_length=128,help_text="Favorite subject")
 	future_dream = models.TextField(null=True,blank=True,help_text="Dreams of your future")
 	parents = models.TextField(null=True,blank=True,help_text="Names of your parents")
 	guardians = models.TextField(null=True,blank=True,help_text="Names of your guardians")
