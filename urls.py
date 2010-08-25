2dd90c3eb67fbde209c21cd2401eeb1ee11f8378
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-08-25 23:07:52 +0300

    Sitemap incorporation for indexation used by google-search engine, remaining todo & backed-up settings

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
-
+from cms.sitemaps import CMSSitemap
 from apps.news import urls as news_urls
 admin.autodiscover()
 
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {'cmspages': CMSSitemap}}),
     (r'^js/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static/javascripts'}),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
     (r'^admin/', include(admin.site.urls)),
